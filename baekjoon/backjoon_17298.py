3c1613d178fba92cf0fbf19721889c3aef419dc5
SWARTHYPEARL <swarthypearl@gmail.com>
2022-10-11 23:10:27 +0900

    new study & not solved yet

---
+++ baekjoon/backjoon_17298.py
@@ -0,0 +1,15 @@
+
+# link: https://www.acmicpc.net/problem/17298
+
+def solution():
+    import sys
+    input = sys.stdin.readline
+
+    seq_size = int(input())
+    seq_list = list(map(int, input().split()))
+
+    for target_num in reversed(seq_list):
+        pass
+
+if __name__ == "__main__":
+    solution()
