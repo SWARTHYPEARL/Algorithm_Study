0402eaae26d89f525ef4071787b18e23f382953f
SWARTHYPEARL <swarthypearl@gmail.com>
2022-10-13 13:03:16 +0900

    study solved

--- baekjoon/backjoon_17298.py
+++ baekjoon/backjoon_17298.py
@@ -1,15 +1,47 @@
 
-# link: https://www.acmicpc.net/problem/17298
+# link: https://www.acmicpc.ne1t/problem/17298
+# Level: G4
+# 스택의 특성을 이용한 센스
 
 def solution():
     import sys
     input = sys.stdin.readline
 
     seq_size = int(input())
-    seq_list = list(map(int, input().split()))
+    seq_list = tuple(map(int, input().split()))
 
-    for target_num in reversed(seq_list):
-        pass
+    from collections import deque
+    seq_stack = deque()
+    output_stack = deque()
+    for target_idx, target_num in enumerate(seq_list):
+        while seq_stack and seq_stack[-1][1] < target_num:
+            pop_idx = seq_stack.pop()[0]
+            output_stack[pop_idx] = target_num
+
+        seq_stack.append((target_idx, target_num))
+        output_stack.append(-1)
+                
+    print(*output_stack)
+
+def solution_2nd():
+    import sys
+    input = sys.stdin.readline
+    seq_size = int(input())
+    seq_list = tuple(map(int, input().split()))
+
+    from collections import deque
+    seq_stack = deque()
+    output_list = [-1] * seq_size
+    for target_idx, target_num in enumerate(seq_list):
+        while seq_stack and seq_stack[-1][1] < target_num:
+            pop_idx = seq_stack.pop()[0]
+            output_list[pop_idx] = target_num
+            
+        seq_stack.append((target_idx, target_num))
+                
+    print(*output_list)
+
 
 if __name__ == "__main__":
-    solution()
+    #solution()
+    solution_2nd()
